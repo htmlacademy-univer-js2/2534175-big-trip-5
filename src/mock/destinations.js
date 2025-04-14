bb5a688a0b3fd6990013cfab4b892a38215ad12c
Re1axSs <147532064+Re1axSs@users.noreply.github.com>
2025-04-14 15:49:46 +0500

    Create destinations.js

---
+++ src/mock/destinations.js
@@ -0,0 +1,160 @@
+const mockDestinations = [
+  {
+        "id": "dc4f0e19-085f-4d22-8171-d77595b838f1",
+        "description": "Kopenhagen - for those who value comfort and coziness",
+        "name": "Kopenhagen",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/6.jpg",
+            "description": "Kopenhagen a true asian pearl"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/18.jpg",
+            "description": "Kopenhagen with crowded streets"
+          }
+        ]
+      },
+      {
+        "id": "d238939a-eebf-4632-b596-eb44443ea8ee",
+        "description": "Geneva - in a middle of Europe",
+        "name": "Geneva",
+        "pictures": []
+      },
+      {
+        "id": "c1abe981-4d5a-4fa3-be4f-10b46c5e6822",
+        "description": "Hiroshima - in a middle of Europe",
+        "name": "Hiroshima",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/12.jpg",
+            "description": "Hiroshima full of of cozy canteens where you can try the best coffee in the Middle East"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/2.jpg",
+            "description": "Hiroshima with an embankment of a mighty river as a centre of attraction"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/17.jpg",
+            "description": "Hiroshima with a beautiful old town"
+          }
+        ]
+      },
+      {
+        "id": "88630820-2212-419e-95a0-cca18751f641",
+        "description": "Nagasaki - a perfect place to stay with a family",
+        "name": "Nagasaki",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/12.jpg",
+            "description": "Nagasaki is a beautiful city"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/13.jpg",
+            "description": "Nagasaki with crowded streets"
+          }
+        ]
+      },
+      {
+        "id": "bb3f10a5-c3d4-4669-963b-08a7101658de",
+        "description": "Amsterdam - with crowded streets",
+        "name": "Amsterdam",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/3.jpg",
+            "description": "Amsterdam a perfect place to stay with a family"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/2.jpg",
+            "description": "Amsterdam is a beautiful city"
+          }
+        ]
+      },
+      {
+        "id": "4a97e465-747e-4e5e-a2e3-a87ef5efc15b",
+        "description": "Venice - with crowded streets",
+        "name": "Venice",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/2.jpg",
+            "description": "Venice for those who value comfort and coziness"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/10.jpg",
+            "description": "Venice full of of cozy canteens where you can try the best coffee in the Middle East"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/12.jpg",
+            "description": "Venice for those who value comfort and coziness"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/5.jpg",
+            "description": "Venice with crowded streets"
+          }
+        ]
+      },
+      {
+        "id": "fbeda7ac-a39e-463e-bcb1-7412314a635b",
+        "description": "Munich - for those who value comfort and coziness",
+        "name": "Munich",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/19.jpg",
+            "description": "Munich with crowded streets"
+          }
+        ]
+      },
+      {
+        "id": "14b9a819-5988-4bb9-81f5-b5610aa12319",
+        "description": "Saint Petersburg - is a beautiful city",
+        "name": "Saint Petersburg",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/8.jpg",
+            "description": "Saint Petersburg famous for its crowded street markets with the best street food in Asia"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/1.jpg",
+            "description": "Saint Petersburg in a middle of Europe"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/7.jpg",
+            "description": "Saint Petersburg in a middle of Europe"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/8.jpg",
+            "description": "Saint Petersburg middle-eastern paradise"
+          }
+        ]
+      },
+      {
+        "id": "d9999450-8851-405d-ad67-994fc34a2c65",
+        "description": "Frankfurt - middle-eastern paradise",
+        "name": "Frankfurt",
+        "pictures": []
+      },
+      {
+        "id": "db63adc8-5b00-4715-9549-b120d5b13dc0",
+        "description": "Vien - full of of cozy canteens where you can try the best coffee in the Middle East",
+        "name": "Vien",
+        "pictures": [
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/19.jpg",
+            "description": "Vien a perfect place to stay with a family"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/8.jpg",
+            "description": "Vien with crowded streets"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/6.jpg",
+            "description": "Vien famous for its crowded street markets with the best street food in Asia"
+          },
+          {
+            "src": "https://24.objects.htmlacademy.pro/static/destinations/1.jpg",
+            "description": "Vien is a beautiful city"
+          }
+        ]
+    }
+];
+
+export {mockDestinations};
