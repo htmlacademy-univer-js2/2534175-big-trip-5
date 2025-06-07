e432de48084065d351218a4205ecf5b95fc3296f
Re1axSs <147532064+Re1axSs@users.noreply.github.com>
2025-06-07 16:11:18 +0500

    Update trip-filters-view.js

--- src/view/trip-filters-view.js
+++ src/view/trip-filters-view.js
@@ -1,42 +1,31 @@
-import AbstractView from "../framework/view/abstract-view.js";
+import AbstractView from "../framework/view/abstract-view.js"; 
 
-function createFiltersTemplate() {
-  return `<div class="trip-main__trip-controls  trip-controls">
-            <div class="trip-controls__filters">
-              <h2 class="visually-hidden">Filter events</h2>
-              <form class="trip-filters" action="#" method="get">
-                <div class="trip-filters__filter">
-                  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-                  <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-                </div>
-
-                <div class="trip-filters__filter">
-                  <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-                  <label class="trip-filters__filter-label" for="filter-future">Future</label>
-                </div>
-
-                <div class="trip-filters__filter">
-                  <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-                  <label class="trip-filters__filter-label" for="filter-present">Present</label>
-                </div>
-
-                <div class="trip-filters__filter">
-                  <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-                  <label class="trip-filters__filter-label" for="filter-past">Past</label>
-                </div>
-
-                <button class="visually-hidden" type="submit">Accept filter</button>
-              </form>
-            </div>
-          </div>
-
-          <button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>
-        </div>
-      </div>`;
+function createFiltersTemplate(filters) {
+  return `<div class="trip-controls__filters">
+    <h2 class="visually-hidden">Filter events</h2>
+    <form class="trip-filters" action="#" method="get">
+      ${filters.map(({ type, name, isDisabled }, index) => `
+        <div class="trip-filters__filter">
+          <input id="filter-${type}" class="trip-filters__filter-input visually-hidden"
+                 type="radio" name="trip-filter" value="${type}"
+                 ${index === 0 ? 'checked' : ''} ${isDisabled ? 'disabled' : ''}>
+          <label class="trip-filters__filter-label" for="filter-${type}">${name}</label>
+        </div>`).join('')}
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>
+  </div>
+  <button class="trip-main__event-add-btn btn btn--big btn--yellow" type="button">New event</button>`;
 }
 
-export default class TripFiltersView extends AbstractView{
+export default class TripFiltersView extends AbstractView {
+  #filters = [];
+
+  constructor(filters) {
+    super();
+    this.#filters = filters;
+  }
+
   get template() {
-    return createFiltersTemplate();
+    return createFiltersTemplate(this.#filters);
   }
 }
