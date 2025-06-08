45965f641f9203cddb4ff8e9cc770c75ec88222c
Re1axSs <147532064+Re1axSs@users.noreply.github.com>
2025-06-08 16:51:17 +0500

    Update sort.js

--- src/mock/sort.js
+++ src/mock/sort.js
@@ -1,14 +1,13 @@
 export const SORT_TYPES = {
-    DAY: 'sort-day',
-    TIME: 'sort-time',
-    PRICE: 'sort-price'
-  };
-  
-  export const generateSortItems = () => ([
-    { type: SORT_TYPES.DAY, name: 'Day', isDisabled: false },
-    { type: 'sort-event', name: 'Event', isDisabled: true },
-    { type: SORT_TYPES.TIME, name: 'Time', isDisabled: false },
-    { type: SORT_TYPES.PRICE, name: 'Price', isDisabled: false, isChecked: true },
-    { type: 'sort-offer', name: 'Offers', isDisabled: true }
-  ]);
-  
+  DAY: 'sort-day',
+  TIME: 'sort-time',
+  PRICE: 'sort-price'
+};
+
+export const generateSortItems = () => ([
+  { type: SORT_TYPES.DAY, name: 'Day', isDisabled: false, isChecked: true },
+  { type: 'sort-event', name: 'Event', isDisabled: true },
+  { type: SORT_TYPES.TIME, name: 'Time', isDisabled: false },
+  { type: SORT_TYPES.PRICE, name: 'Price', isDisabled: false },
+  { type: 'sort-offer', name: 'Offers', isDisabled: true }
+]);
