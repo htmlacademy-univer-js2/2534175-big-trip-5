1cb2888b610b4233f235579478eecc319ff0c9b0
Re1axSs <147532064+Re1axSs@users.noreply.github.com>
2025-06-09 19:28:29 +0500

    Update empty-list-view.js

--- src/view/empty-list-view.js
+++ src/view/empty-list-view.js
@@ -2,17 +2,18 @@ import AbstractView from '../framework/view/abstract-view.js';
 
 const NoPointTextType = {
   everything: 'Click New Event to create your first point',
-  past: 'There are no past events now',
-  present: 'There are no present events now',
   future: 'There are no future events now',
+  present: 'There are no present events now',
+  past: 'There are no past events now'
 };
 
 function createEmptyListTemplate(filterType) {
-  return `<p class="trip-events__msg">${NoPointTextType[filterType]}</p>`;
+  const text = NoPointTextType[filterType] || NoPointTextType.everything;
+  return `<p class="trip-events__msg">${text}</p>`;
 }
 
 export default class EmptyListView extends AbstractView {
-  #filterType;
+  #filterType = null;
 
   constructor({filterType}) {
     super();
