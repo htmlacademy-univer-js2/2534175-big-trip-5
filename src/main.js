825b34863623d3b12e0b0fd5b351a48c8b61bb0c
Re1axSs <147532064+Re1axSs@users.noreply.github.com>
2025-06-09 19:31:09 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
 import TripPresenter from './presenter/trip-presenter.js';
-import TripFiltersView from './view/trip-filters-view.js';
+import PointModel, {FiltersModel} from './model/model.js';
 import {render} from './framework/render.js';
-import PointModel from './model/model.js';
-import {generateFilterItems} from './mock/filter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 
-const filtersElement = document.querySelector('.trip-controls__filters');
-const tripEvents = document.querySelector('.trip-events');
+const siteMainElement = document.querySelector('.trip-events');
+const siteHeaderElement = document.querySelector('.trip-main');
+const siteFiltersElement = document.querySelector('.trip-controls__filters');
+const newPointButton = document.querySelector('.trip-main__event-add-btn');
 
 const pointsModel = new PointModel();
+const filtersModel = new FiltersModel();
+
 const tripPresenter = new TripPresenter({
-  tripContainer: tripEvents,
-  pointsModel: pointsModel,
+  container: siteMainElement,
+  pointsModel,
+  filtersModel,
+  newPointButton
 });
 
-const filterItems = generateFilterItems(pointsModel.getPoints());
-render(new TripFiltersView(filterItems), filtersElement);
+const filterPresenter = new FilterPresenter({
+  filterContainer: siteFiltersElement,
+  filtersModel,
+  pointsModel
+});
 
+filterPresenter.init();
 tripPresenter.init();
